824906e34d5a87367d877bedab572d986df0613f
dev <dev@wsx16.mfti>
2020-01-10 16:40:26 +0100

    refactoring

--- SelectPresentator/src/MenuSelection.java
+++ SelectPresentator/src/MenuSelection.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.List;
-import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MenuSelection {
@@ -15,28 +14,35 @@ public class MenuSelection {
         try {
             do {
                 menuSelection = in.nextInt();
-                Map<Integer, Action> map = new HashMap<Integer, Action>() {
-                    {
-                        put(1, new AddPersonToList(in, _fileName));
-                        put(2, new Remove(in,_fileName));
-                        put(3, new SelectRandomPresentator(in, _fileName));
-                    }
-                };
-                map.get(menuSelection).exec();
+                
+                switch (menuSelection) {
+                    case 1:
+                        addPresonToList(in);
+                        break;
+                    case 2:
+                        removePersonFromList(in);
+                        break;
+                    case 3:
+                        selectRandomPresentator();
+                        break;
+                    default:
+                        break;
+                }
             } while (menuSelection > 0 && menuSelection < 4);
         } catch (InputMismatchException e) {
-            System.out.println(e);
+            System.out.println("End..");
         }
         in.close();
     }
 
     public List<Person> viewParticipantList() {
-        ArrayList<Person> _participiants = new ReadCsvFile(_fileName).readFile();
+        ArrayList<Person> _participiants = new Store(_fileName).readParticipiantList();
 
         System.out.println("Participiants list:\n");
         for (Person person : _participiants) {
             if (!person.isAbsent()) {
-                System.out.println(person.getIndex() + " " + person.getFullName() + " " + person.getIndex() + " is absent " + person.isAbsent());
+                System.out.println(person.getIndex() + " " + person.getFullName() + " " + person.getIndex()
+                    + " is absent " + person.isAbsent());
             }
         }
         return _participiants;
@@ -51,5 +57,72 @@ public class MenuSelection {
                 + "2. Remove person from the participiants list\n"
                 + "3. Choose random presentator\n");
         System.out.println("---------------------------------------------");
+    }
+
+    public void addPresonToList(Scanner in) {
+        String fullName;
+        ArrayList<Person> _participiants = new Store(_fileName).readParticipiantList();
+        do {
+            in = new Scanner(System.in);
+            System.out.println("Insert participiant full name (Press q to exit):");
+            fullName = in.nextLine();
+            if (!fullName.equals("q")) {
+                String[] result = fullName.split(" ");
+                String firstName = result[0];
+                String lastName = "";
+                if (result.length > 1) {
+                    lastName = result[1];
+                }
+                Person person = new Person(_participiants.size() + 1, firstName, lastName, false);
+                _participiants.add(person);
+            }
+        } while (!fullName.equals("q"));
+        String[] csvColumnNames = { "firstName", "lastName", "isAbsent" };
+        new Store(_fileName).writeParticipiantList(csvColumnNames, _participiants);
+        new MenuSelection().showMenuOptions();
+    }
+
+    public void removePersonFromList(Scanner in) {
+        new MenuSelection().viewParticipantList();
+
+        System.out.println("\nChoose the index of the person you want to exclude from the participiants list!");
+        int index = 0;
+        ArrayList<Person> _participiants = new Store(_fileName).readParticipiantList();
+        try {
+            do {
+                _participiants = new Store(_fileName).readParticipiantList();
+                in = new Scanner(System.in);
+                index = in.nextInt();
+
+                for (Person person : _participiants) {
+                    if (person.getIndex() == index) {
+                        person.setIndex(0);
+                        person.setAbsent(true);
+                    }
+                }
+                String[] csvColumnNames = { "firstName", "lastName", "isAbsent" };
+                new Store(_fileName).writeParticipiantList(csvColumnNames, _participiants);
+                new MenuSelection().viewParticipantList();
+
+            } while (index > 0 && index < _participiants.size());
+        } catch (InputMismatchException e) {
+            new MenuSelection().showMenuOptions();
+        }
+        new MenuSelection().showMenuOptions();
+    }
+
+    public void selectRandomPresentator() {
+        ArrayList<Person> _participiants = new Store(_fileName).readParticipiantList();
+        Random randomGenerator = new Random();
+        ArrayList<Person> presentParticipiant = new ArrayList<>();
+        for (Person person : _participiants) {
+            if (!person.isAbsent()) {
+                presentParticipiant.add(person);
+            }
+        }
+        int index = randomGenerator.nextInt(presentParticipiant.size());
+        Person randomPresentator = presentParticipiant.get(index);
+        System.out.println("random Presentor " + randomPresentator.getFullName() + "\n");
+        new MenuSelection().showMenuOptions();
     }
 }
--- SelectPresentator/src/Store.java
+++ SelectPresentator/src/Store.java
@@ -1,16 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ReadCsvFile {
+public class Store {
+    
     private final String _fileName;
+    private String _csvColumnNames = "";
+    private final String _COMMA_DELIMITER = ",";
+    private final String _NEW_LINE_SEPARATOR = "\n";
 
-    public ReadCsvFile(String fileName) {
-        _fileName = fileName;  
+    public Store(String fileName) {
+        _fileName = fileName;
     }
-    public ArrayList<Person> readFile() {
+
+    public ArrayList<Person> readParticipiantList() {
         ArrayList<Person> participiants = new ArrayList<>();
         BufferedReader csvReader = null;
         try {
@@ -24,7 +31,7 @@ public class ReadCsvFile {
             int index = 0;
             while ((row = csvReader.readLine()) != null) {
                 String attributes[] = row.split(",");
-                
+
                 String firstName = attributes[0];
                 String lastName = attributes[1];
                 Boolean isAbsent = Boolean.parseBoolean(attributes[2]);
@@ -44,5 +51,31 @@ public class ReadCsvFile {
             throw new RuntimeException(e);
         }
         return participiants;
+    }
+    
+    public void writeParticipiantList(String[] csvColumnNames, List<Person> participiants) {
+
+        for (int i = 0; i < csvColumnNames.length; i++) {
+            if ((i + 1) < csvColumnNames.length) {
+                _csvColumnNames += csvColumnNames[i] + _COMMA_DELIMITER;
+            } else {
+                _csvColumnNames += csvColumnNames[i] + _NEW_LINE_SEPARATOR;
+            }
+        }
+        try {
+            FileWriter fw = new FileWriter(_fileName);
+            fw.write(_csvColumnNames);
+            for (Person person : participiants) {
+                fw.append(person.getFirstName());
+                fw.append(_COMMA_DELIMITER);
+                fw.append(person.getLastName());
+                fw.append(_COMMA_DELIMITER);
+                fw.append(String.valueOf(person.isAbsent()));
+                fw.append(_NEW_LINE_SEPARATOR);
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
